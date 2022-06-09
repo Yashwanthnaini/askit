0cd268c26d521425bdbc0617b77d571fe483ac26
Naini Yashwanth <nainiyashwanth@gmail.com>
2022-06-09 16:19:38 +0530

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_PROD_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
