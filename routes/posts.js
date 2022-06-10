1460dc1ad08df3a5ba4b179c0884bdd30857818e
Naini Yashwanth <nainiyashwanth@gmail.com>
2022-06-10 16:24:04 +0530

    post api updated

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,32 @@
 const {Post,validatePost} = require("../models/postModel");
+const auth = require("../middleware/authorization");
 const {User} = require("../models/userModel");
 const express = require("express");
 const router = express.Router();
 
+router.get("/:pagesize/:pagenum", async(req,res)=>{
+    const pagesize = req.params.pagesize
+    const pagenum = req.params.pagenum
+    try{
+        const posts = await Post
+                            .find()
+                            .sort("date")
+                            .select("-author._id __v")
+                            .skip(pagenum-1*pagesize)
+                            .limit(pagesize);
+        res.send({posts});                    
+    }
+    catch(ex){
+        console.error(ex);
+        res.status(500).json({
+            error: "something went wrong try after some time!", 
+        });
+    }
+});
 
-router.post("/add", async(req, res)=>{
+router.get("/my/posts")
+
+router.post("/add", auth, async(req, res)=>{
     try{
         const {error} = validatePost(req.body);
         if (error) return res.status(400).send(error.details[0].message);
