920665e85b13bd15b65b05cc3c3809e8b3a87164
Naini Yashwanth <nainiyashwanth@gmail.com>
2022-06-20 19:05:21 +0530

    bug removed

--- routes/answers.js
+++ routes/answers.js
@@ -1,4 +1,5 @@
 const {Answer, validateAnswer } = require("../models/answerModel");
+const {Question} = require("../models/question");
 const auth = require("../middleware/authorization");
 const {User} = require("../models/userModel");
 const express = require("express");
@@ -13,8 +14,8 @@ router.post("/add", auth, async (req,res) => {
         const user = await User.findById(req.user._id);
         if (!user) return res.status(400).send("Invalid user.");
 
-        const post = await Question.findById(req.body.postId);
-        if(!post) return res.status(400).send("Invalid post");
+        const question = await Question.findById(req.body.questionId);
+        if(!question) return res.status(400).send("Invalid Question");
 
         const answer = new Answer ({
             answer: req.body.answer,
@@ -22,17 +23,17 @@ router.post("/add", auth, async (req,res) => {
                 _id: user._id,
                 name : user.name
             },
-            post:{
-                _id: req.body.QuestionId
+            question:{
+                _id: req.body.questionId
             }
         })
         await answer.save();
         const notification = new Notification ({
-            notification : `${user.name} answered to your question ${post.title}`,
+            notification : `${user.name} answered to your question ${question.title}`,
             user : {
-                _id : post.author._id
+                _id : question.author._id
             },
-            link : `https://askito.herokuapp.com/api/questions/${post._id}`
+            link : `https://askito.herokuapp.com/api/questions/${question._id}`
         })
         await notification.save();
         res.json({
