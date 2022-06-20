47aa98e7d0957006c9a3d9359f9a54455c6e4e5b
Naini Yashwanth <nainiyashwanth@gmail.com>
2022-06-20 16:41:45 +0530

    bug removed

--- routes/posts.js
+++ routes/posts.js
@@ -135,8 +135,8 @@ router.put("/edit/title/:id", auth, async (req, res) => {
         if(post.author._id !== req.user._id){
             return res.status(401).send("access denied.");
         }
-
-        await Post.findByIdAndUpdate(req.params.id, {
+        const id = mongoose.Types.ObjectId.fromString(req.params.id);
+        await Post.findByIdAndUpdate(id, {
             title: req.body.title
         }, {new: true});
         res.send(post);
