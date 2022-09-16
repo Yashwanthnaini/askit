3d9f461dfff03851a6960cce5cfcdc22ae11744e
Naini Yashwanth <Nainiyashwanth@gmail.com>
2022-09-16 21:34:17 +0530

    Update answerMail.js

--- resources/answerMail.js
+++ resources/answerMail.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 module.exports = async function(email,token,name,nametwo) {
@@ -6,7 +7,8 @@ module.exports = async function(email,token,name,nametwo) {
       host: "smtp-relay.sendinblue.com",
       port: 587,
       auth: {
-        
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
     const senderAddress = "nainiyashwanth@gmail.com";
